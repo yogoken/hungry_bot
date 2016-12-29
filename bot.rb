1ad7bdf5744382485ac17d2ceb74dfa12faa9018
yogoken <yogo.k@husky.neu.edu>
2016-12-29 10:33:04 +0900

    Update aiueo to bot

--- bot.rb
+++ bot.rb
@@ -18,7 +18,7 @@ client.on :message do |data|
   case data['text']
   when "おなかすいた" then
     client.message channel: data['channel'], text: "<@#{data['user']}>さん\n<@yogoken>さんが一緒に食べる人探しています!!\n ■オススメのお店3選\n→吉野家: https://tabelog.com/tokyo/A1301/A130102/13194113/\n→小洞天: https://retty.me/area/PRE13/ARE2/SUB202/100000011274/\n→麺屋ひょっとこ: https://retty.me/area/PRE13/ARE2/SUB202/100000018958/\n■場所\n5Fエレベーター前\n■待ち合わせ時間\n2分後"
-  when /^aiueo/ then
+  when /^bot/ then
     client.message channel: data['channel'], text: "Sorry <@#{data['user']}>, what?"
   end
 end
