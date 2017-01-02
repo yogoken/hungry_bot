4a05e562cf98ea0b8b91db22e1085801a2f34dd9
yogoken <yogo.k@husky.neu.edu>
2017-01-02 14:21:00 +0900

    Update engineer.rb

--- engineer.rb
+++ engineer.rb
@@ -10,7 +10,7 @@ ActiveRecord::Base.establish_connection(config['development'])
 
 Slack.configure do |conf|
   # 先ほど控えておいたAPI Tokenをセット
-  conf.token = 'xoxb-121387490661-trbXKpDNblw6IsoLMEZixu5j'
+  conf.token = 'xoxb-121387490661-RkDYrGCVQRrOWMxHzj4kRbkh'
 end
 
 # RTM Clientのインスタンスを生成
@@ -19,6 +19,7 @@ client = Slack::RealTime::Client.new
 # hello eventを受け取った時の処理
 client.on :hello do
   puts 'connected!'
+  client.message channel: "C3KV3TLH5", text: "Welcome to engineer quiz channel!!\nこのchannelでは少しでも早く、多くのFincエンジニアの名前を覚えてもらうために\nランダムで社員、インターン生の写真から名前を当ててもらうクイズが作成されてます\nそれでは *start* で始めましょう!!"
 end
 
 # message eventを受け取った時の処理
@@ -36,6 +37,7 @@ client.on :message do |data|
       @engineer = Engineer.first.image
       p @engineer
     end
+    p data
     @answer = Engineer.order("RAND()").first
     client.message channel: data['channel'],
     text: "#{@answer.image}"
@@ -44,8 +46,17 @@ client.on :message do |data|
         client.message channel: data2['channel'], text: '正解！'
         break
       end
-      if data2['text'] != "#{@answer.nickname}" && data2['text'] != data['text'] && data2['subtype'] != 'bot_message' && data2['text'] != nil
-        client.message channel: data2['channel'], text: '残念！'
+      if data2['text'] != "#{@answer.nickname}" && data2['text'] != data['text'] && data2['subtype'] != 'bot_message' && data2['text'] != nil && data2['text'] != 'start'
+        client.message channel: data2['channel'], text: '残念！あと1回しか間違えられないよ!!'
+        client.on :message do |data3|
+          if data3['text'] == "#{@answer.nickname}"
+            client.message channel: data3['channel'], text: '正解っす!!'
+            break
+          elsif data3['text'] != "#{@answer.nickname}" && data3['text'] != data2['text'] && data3['subtype'] != 'bot_message' && data3['text'] != nil && data3['text'] != 'start'
+            client.message channel: data3['channel'], text: '残念！5回タイプして覚えよう!!正解は"#{@answer.nickname}"さんでした'
+            break
+          end
+        end
         break
       end
     end
